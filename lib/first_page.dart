2bf98cb3364d802da89f2d1806203ad88b9c0712
Reihan Wudd Hibatullah <116327302+ninkyuwudd@users.noreply.github.com>
2023-08-13 18:27:44 +0700

    empty project

--- lib/first_page.dart
+++ lib/first_page.dart
@@ -1,80 +1,16 @@
 import 'package:flutter/material.dart';
 
-class FirstPage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
   const FirstPage({super.key});
 
   @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  int count = 0;
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: Container(
-          width: MediaQuery.of(context).size.width,
-          child: Column(
-            children: [
-              TextButton(onPressed: () {}, child: const Text("textButton")),
-              ElevatedButton(
-                  onPressed: () {}, child: const Text("elevatedButton")),
-              OutlinedButton(
-                  onPressed: () {}, child: const Text("outlineButton")),
-              Container(
-                width: MediaQuery.of(context).size.width,
-                height: 100,
-                child: ElevatedButton.icon(
-                  label: Text("nice"),
-                  icon: Icon(Icons.add),
-                  style: ButtonStyle(
-                    foregroundColor:
-                        MaterialStateProperty.all<Color>(Colors.blue),
-                    overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                      (Set<MaterialState> states) {
-                        if (states.contains(MaterialState.hovered))
-                          return Colors.blue.withOpacity(0.04);
-                        if (states.contains(MaterialState.focused) ||
-                            states.contains(MaterialState.pressed))
-                          return Colors.blue.withOpacity(0.12);
-                        return null; // Defer to the widget's default.
-                      },
-                    ),
-                  ),
-                  onPressed: () {},
-                ),
-              ),
-              Container(
-                width: MediaQuery.of(context).size.width / 2,
-                height: MediaQuery.of(context).size.height / 10,
-                child: ElevatedButton(
-                    style: ButtonStyle(
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.white),
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.blue),
-                        overlayColor: MaterialStateProperty.all<Color>(
-                            const Color.fromARGB(40, 255, 193, 7))),
-                    onPressed: () {},
-                    child: const Text("Elevated")),
-              ),
-              TextButton(
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue),
-                ),
-                onPressed: () {},
-                child: Text('TextButton'),
-              ),
-              TextButton(
-                style: ButtonStyle(
-                  overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                      (Set<MaterialState> states) {
-                    if (states.contains(MaterialState.focused))
-                      return Colors.red;
-                    return null; // Defer to the widget's default.
-                  }),
-                ),
-                onPressed: () {},
-                child: Text('TextButton'),
-              ),
-            ],
-          ),
-        ));
+    return Scaffold(appBar: AppBar(), body: Container());
   }
 }
